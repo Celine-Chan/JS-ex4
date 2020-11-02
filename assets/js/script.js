4722bdc6cd395fae731e81126745ad767265c2cd
Celine_lhp5 <celine.louvel.76@outlook.fr>
2020-11-02 11:34:15 +0100

    modif ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 let userName = prompt('Nom :',);
 let userAge = prompt('Age :');
 let userCity = prompt('Ville :');
-    alert("Nom : " + userName + " Age : " + userAge + " Ville : " + userCity);
+    alert("Nom : " + userName + " \nAge : " + userAge + " \nVille : " + userCity);
+    // \n = saut de ligne en js
